ee65a8b042a922c460adb41110de23e7528a690e
Juan Pablo Lucumi Medina <lucumi763@gmail.com>
2024-03-03 22:40:48 -0500

    Add script for DataTable

---
+++ ShoppingCart.Web/wwwroot/js/Category.js
@@ -0,0 +1,44 @@
+﻿var dtable;
+$(document).ready(function () {
+    dtable = $('#myTableC').DataTable({
+        "ajax": { "url": "/Admin/Category/AllCategories" },
+        "columns": [
+            { "data": "name" },
+            { "data": "displayOrder" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<a href="/Admin/Category/CreateUpdate?id=${data}">Editar</a>
+                            <a onclick=RemoveCategory("/Admin/Category/Delete/${data}") style="cursor:pointer;">Eliminar</a>`
+                }
+            }
+        ]
+    });
+});
+
+function RemoveCategory(url) {
+    Swal.fire({
+        title: 'Estas seguro?',
+        text: 'Esta accion es irreversible',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#8b0000',
+        cancelButtonColor: '#808080',
+        confirmButtonText: 'Si, Estoy Seguro'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    if (data.success) {
+                        dtable.ajax.reload();
+                        toastr.success(data.success)
+                    } else {
+                        toastr.error(data.success)
+                    }
+                }
+            })
+        }
+    })
+}
