fe948b0f38213cda5e237f7874f20b4c97629c09
Juan Pablo Lucumi Medina <lucumi763@gmail.com>
2024-03-05 08:52:47 -0500

    Add Repos OrderHeader & OrderDetail

---
+++ ShoppingCart.DataAccess/Repositories/OrderDetailRepository.cs
@@ -0,0 +1,31 @@
+﻿using ShoppingCart.DataAccess.Data;
+using ShoppingCart.DataAccess.Repositories.IRepositories;
+using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.DataAccess.Repositories
+{
+    public class OrderDetailRepository : Repository<OrderDetail> , IOrderDetailRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public OrderDetailRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public void Update(OrderDetail orderDetail)
+        {
+            _context.OrderDetails.Update(orderDetail);
+            //var orderDetailDB = _context.OrderDetails.FirstOrDefault(x=>x.Id == orderDetail.Id);
+            //if (orderDetailDB != null) 
+            //{
+            //    orderDetailDB.Price = orderDetail.Price;
+            //    ...
+            //}
+        }
+    }
+}
---
+++ ShoppingCart.DataAccess/Repositories/OrderHeaderRepository.cs
@@ -0,0 +1,47 @@
+﻿using ShoppingCart.DataAccess.Data;
+using ShoppingCart.DataAccess.Repositories.IRepositories;
+using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.DataAccess.Repositories
+{
+    public class OrderHeaderRepository : Repository<OrderHeader> , IOrderHeaderRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderHeaderRepository(ApplicationDbContext context) : base(context) 
+        {
+            _context = context;
+        }
+
+        public void Update(OrderHeader orderHeader)
+        {
+            _context.OrderHeaders.Update(orderHeader);
+        }
+
+        public void UpdateStatus(int Id, string orderStatus, string? paymentStatus = null)
+        {
+            var order = _context.OrderHeaders.FirstOrDefault(x  => x.Id == Id);
+            if (order != null) 
+            {
+                order.OrderStatus = orderStatus;
+            }
+            if (paymentStatus != null) 
+            {
+                order.PaymentStatus = paymentStatus;
+            }
+        }
+
+        public void PaymentStatus(int Id, string sessionId, string paymentIntentId)
+        {
+            var orderHeader = _context.OrderHeaders.FirstOrDefault(x => x.Id == Id);
+            orderHeader.DateOfPayment = DateTime.Now;
+            orderHeader.PaymentIntentId = paymentIntentId;
+            orderHeader.SessionId = sessionId;
+        }
+    }
+}
