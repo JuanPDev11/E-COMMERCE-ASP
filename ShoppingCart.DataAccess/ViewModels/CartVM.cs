6074a42609a32e97d2ed8bd71151896085fbc529
Juan Pablo Lucumi Medina <lucumi763@gmail.com>
2024-03-06 04:24:05 -0500

    Add Controller-View for Model-Cart

---
+++ ShoppingCart.DataAccess/ViewModels/CartVM.cs
@@ -0,0 +1,18 @@
+﻿using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.DataAccess.ViewModels
+{
+    public class CartVM
+    {
+        
+        public IEnumerable<Cart> Carts { get; set; } 
+
+        public OrderHeader OrderHeader { get; set; }
+    }
+}
+
---
+++ ShoppingCart.Web/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.DataAccess.Repositories;
+using ShoppingCart.DataAccess.Repositories.IRepositories;
+using ShoppingCart.DataAccess.ViewModels;
+using ShoppingCart.Models;
+using System.Security.Claims;
+
+namespace ShoppingCart.Web.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    [Authorize]
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CartVM vm { get; set; }
+
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            var claimsidentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsidentity.FindFirst(ClaimTypes.NameIdentifier);
+            vm = new CartVM()
+            {
+                Carts = _unitOfWork.Cart.GetAll(u => u.ApplicationUserId == claim.Value, includeProperties: "Product"),
+                OrderHeader = new OrderHeader()
+            };
+            foreach (var item in vm.Carts)
+            {
+                vm.OrderHeader.OrderTotal += (item.Product.Price * item.Count);
+            }
+
+            return View(vm);
+        }
+    }
+}
