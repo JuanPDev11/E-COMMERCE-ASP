d2b16ee4f36108106afa1560d604ddda44d5a48d
Juan Pablo Lucumi Medina <lucumi763@gmail.com>
2024-03-08 20:25:39 -0500

    General Updates

--- ShoppingCart.Web/wwwroot/js/Order.js
+++ ShoppingCart.Web/wwwroot/js/Order.js
@@ -1,1 +1,43 @@
-﻿
+﻿var dtable;
+$(document).ready(function(){
+    var url = window.location.search;
+    if (url.includes("pending")) {
+        OrderTable("pending");
+    }
+    else {
+        if (url.includes("approved")) {
+            OrderTable("approved");
+        }
+        else {
+            if (url.includes("shipped")) {
+                OrderTable("shipped");
+            }
+            else {
+                if (url.includes("underprocess")) {
+                    OrderTable("underprocess");
+                }
+                else {
+                    OrderTable("all");
+                }
+            }
+        }
+    }
+});
+
+function OrderTable(status) {
+    dtable = $('#myTableO').DataTable({
+        "ajax": { "url": "/Admin/Orders/AllOrders?status=" + status },
+        "columns": [
+            { "data": "name" },
+            { "data": "phone" },
+            { "data": "orderStatus"},
+            { "data": "orderTotal" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<a href="/Admin/Orders/OrderDetails?id=${data}"><i class="bi bi-pencil-square"></i></a>`
+                }
+            }
+        ]
+    });
+}
