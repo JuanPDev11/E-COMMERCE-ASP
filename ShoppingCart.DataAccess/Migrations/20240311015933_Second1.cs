192d577aef98c79b0e996e108bf390631962df2a
Juan Pablo Lucumi Medina <lucumi763@gmail.com>
2024-03-10 22:59:53 -0500

    Add Second.1 migration

---
+++ ShoppingCart.DataAccess/Migrations/20240311015933_Second1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShoppingCart.DataAccess.Migrations
+{
+    public partial class Second1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Bid",
+                table: "products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bid",
+                table: "products");
+        }
+    }
+}
