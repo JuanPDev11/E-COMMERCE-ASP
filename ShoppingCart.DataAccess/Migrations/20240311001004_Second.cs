7de30b00b290e9c27607ea323c06b41ea02819ac
Juan Pablo Lucumi Medina <lucumi763@gmail.com>
2024-03-10 22:59:10 -0500

    Add Second migration

---
+++ ShoppingCart.DataAccess/Migrations/20240311001004_Second.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShoppingCart.DataAccess.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Followers",
+                table: "Artists",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalSold",
+                table: "Artists",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Volume",
+                table: "Artists",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Followers",
+                table: "Artists");
+
+            migrationBuilder.DropColumn(
+                name: "TotalSold",
+                table: "Artists");
+
+            migrationBuilder.DropColumn(
+                name: "Volume",
+                table: "Artists");
+        }
+    }
+}
